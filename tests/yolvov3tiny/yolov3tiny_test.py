34271edf1fa6a9b09f8d1ad010da7138fc191aaf
jpfcabral <jpfcabral@gmail.com>
2022-04-23 13:32:55 -0300

    add: temperature log

--- tests/yolvov3tiny/yolov3tiny_test.py
+++ tests/yolvov3tiny/yolov3tiny_test.py
@@ -4,6 +4,13 @@ from pathlib import Path
 from tests.utils import init_test
 from src.models.yolov3tiny.yolov3tiny import YoloV3Tiny
 
+try:
+    from gpiozero import CPUTemperature
+    cpu = CPUTemperature()
+    temp_flag = True
+except:
+    cpu = None
+
 def test_yolvov3tiny_predict():
     init_test()
 
@@ -12,8 +19,10 @@ def test_yolvov3tiny_predict():
                             image_size=(416, 416)
                             )
     
-    print('\ndatetime,image,read_time,predict_time,objects')
+    print('\ndatetime,image,read_time,predict_time,objects,temperature')
     for image in os.listdir("./tests/images/person/images"):
         image_path = os.path.join("./tests/images/person/images/", image)
         objects = yolov3tiny.predict(image_path)
-        print(f'{datetime.now()},{image},{round(yolov3tiny.read_image_time, 3)},{round(yolov3tiny.prediction_time, 3)},{len(objects)}')
+
+        temperature = cpu.temperature if cpu is not None else 0.0
+        print(f'{datetime.now()},{image},{round(yolov3tiny.read_image_time, 3)},{round(yolov3tiny.prediction_time, 3)},{len(objects)},{temperature}')
