5405cf543a05f64fbbe87306c94a844c380947f5
jpfcabral <jpfcabral@gmail.com>
2022-04-23 12:07:22 -0300

    print results

--- tests/yolvov3tiny/yolov3tiny_test.py
+++ tests/yolvov3tiny/yolov3tiny_test.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 from pathlib import Path
 from tests.utils import init_test
 from src.models.yolov3tiny.yolov3tiny import YoloV3Tiny
@@ -11,7 +12,8 @@ def test_yolvov3tiny_predict():
                             image_size=(416, 416)
                             )
     
+    print('\ndatetime,image,read_time,predict_time,objects')
     for image in os.listdir("./tests/images/person/images"):
         image_path = os.path.join("./tests/images/person/images/", image)
         objects = yolov3tiny.predict(image_path)
-        print(objects)
+        print(f'{datetime.now()},{image},{round(yolov3tiny.read_image_time, 3)},{round(yolov3tiny.prediction_time, 3)},{len(objects)}')
