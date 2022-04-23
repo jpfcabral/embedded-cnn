46df643fe359386533873200f5147547d2f7c46e
jpfcabral <jpfcabral@gmail.com>
2022-04-23 12:16:23 -0300

    fix: relative path

--- tests/yolvov3tiny/yolov3tiny_test.py
+++ tests/yolvov3tiny/yolov3tiny_test.py
@@ -7,8 +7,8 @@ from src.models.yolov3tiny.yolov3tiny import YoloV3Tiny
 def test_yolvov3tiny_predict():
     init_test()
 
-    yolov3tiny = YoloV3Tiny(weights_path=r"C:\Users\jpfca\Documents\projetos\embedded-cnn\src\models\yolov3tiny\yolov3-tiny.weights",
-                            config_path=r"C:\Users\jpfca\Documents\projetos\embedded-cnn\src\models\yolov3tiny\yolov3-tiny.cfg",
+    yolov3tiny = YoloV3Tiny(weights_path="src/models/yolov3tiny/yolov3-tiny.weights",
+                            config_path="src/models/yolov3tiny/yolov3-tiny.cfg",
                             image_size=(416, 416)
                             )
     
