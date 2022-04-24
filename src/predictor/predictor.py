8b72c02ecee335cf6e6b252b246597eb0ff9b225
jpfcabral <jpfcabral@gmail.com>
2022-04-24 18:46:37 -0300

    update: grayscale input

--- src/predictor/predictor.py
+++ src/predictor/predictor.py
@@ -14,7 +14,7 @@ class Predictor:
     def __read_image(self, image_path):
         try:
             image = cv2.imread(image_path)
-            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
             return image
         except:
             raise Exception("Error reading image")
@@ -25,9 +25,9 @@ class Predictor:
             self.image = self.__read_image(image_path)
             self.read_image_time = time.time() - self.start_time
             
-            intput = cv2.dnn.blobFromImage(self.image, 1 / 255, self.image_size, crop=False)
-            self.model.setInput(intput)
-            output = self.model.forward(layers if layers else None)
+            blob = cv2.dnn.blobFromImage(self.image, 1 / 255, self.image_size, crop=False)
+            self.model.setInput(blob)
+            output = self.model.forward(layers)
             self.prediction_time = time.time() - (self.read_image_time + self.start_time)
             return output
         except:
